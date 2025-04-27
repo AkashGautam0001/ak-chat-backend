d91cca613bea20c61294c3de54779ed22702c7f8
Akash Gautam <akashgautam01584@gmail.com>
2025-04-28 00:55:09 +0530

    add path-to-regexp dependency and format imports in message.route.js

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { createMessage, getMessages, getUserForSidebar } from "../controllers/message.controller.js";
+import {
+  createMessage,
+  getMessages,
+  getUserForSidebar,
+} from "../controllers/message.controller.js";
 import { upload } from "../middlewares/multer.middlewar.js";
 
 const router = express.Router();
 
-router.get("/users",protectRoute,getUserForSidebar );
-router.get("/:id",protectRoute, getMessages);
-router.post("/send/:id", protectRoute,upload.single("image"), createMessage);
+router.get("/users", protectRoute, getUserForSidebar);
+router.get("/:id?", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, upload.single("image"), createMessage);
 
 export default router;
