d75574873434a5f150b6ffe50107dea2c451325e
Akash Gautam <akashgautam01584@gmail.com>
2025-04-28 23:24:34 +0530

    fix: update server port configuration and adjust CORS origin for production

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import { app as socketApp, server } from "./utils/socket.js";
 
 dotenv.config();
 const app = socketApp;
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -26,26 +25,9 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send({
-//     activeStatus: true,
-//     error: false,
-//   });
-// });
-
-// const __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-
 connectDB()
   .then(() => {
-    const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 5000;
     server.listen(port, () => {
       console.log("Server is running on port " + port);
     });
