19e6302d43b9cb071aaa22e9b07c369085ac6c51
Akash Gautam <akashgautam01584@gmail.com>
2025-04-28 23:01:32 +0530

    fix: comment out production-specific code in index.js

--- src/index.js
+++ src/index.js
@@ -33,15 +33,15 @@ app.use("/api/messages", messageRoutes);
 //   });
 // });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 connectDB()
   .then(() => {
